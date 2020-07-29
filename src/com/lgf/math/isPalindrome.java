e04e8eb71c9039cc2c91be90bfb77b736ba18edc
LGF <lgf768561@163.com>
2020-07-29 21:57:13 +0800

    回文数

---
+++ src/com/lgf/math/isPalindrome.java
@@ -0,0 +1,49 @@
+package com.lgf.math;
+
+public class isPalindrome {
+
+    public static void main(String[] args) {
+        boolean palindrome1 = isPalindrome1(132323231);
+        System.out.println(palindrome1);
+    }
+
+    public static boolean isPalindrome1(int x) {
+
+        if (x < 0){
+            return false;
+        }
+        String xs = Integer.toString(x);
+        char[] chars = xs.toCharArray();
+        int i = 0, y = chars.length-1;
+        while (i < y){
+            if (chars[i] == chars[y]){
+                i++;
+                y--;
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //简单粗暴
+    public boolean isPalindrome2(int x) {
+        String reversedStr = (new StringBuilder(x + "")).reverse().toString();
+        return (x + "").equals(reversedStr);
+    }
+
+
+    //比较两值
+    public boolean isPalindrome3(int x) {
+        if(x < 0)
+            return false;
+        int cur = 0;
+        int num = x;
+        while(num != 0) {
+            cur = cur * 10 + num % 10;
+            num /= 10;
+        }
+        return cur == x;
+    }
+
+}
