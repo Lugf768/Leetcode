d405f68f12933ba45b2ba90c8464fd8521b69071
LGF <lgf768561@163.com>
2021-01-03 09:34:56 +0800

    剪绳子

---
+++ src/com/lgf/jzoffer/chapterTwo/Q14_1.java
@@ -0,0 +1,26 @@
+package com.lgf.jzoffer.chapterTwo;
+
+/**
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1）,
+ * 每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
+ * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ */
+public class Q14_1 {
+
+    //解题思路：数学推导/贪心思想
+    //https://leetcode-cn.com/problems/jian-sheng-zi-lcof/solution/mian-shi-ti-14-i-jian-sheng-zi-tan-xin-si-xiang-by/
+    public int cuttingRope(int n) {
+        if (n <= 3){
+            return n - 1;
+        }
+        int a = n / 3, b = n % 3;
+        if (b == 0) {
+            return (int) Math.pow(3, a);
+        }
+        if (b == 1) {
+            return (int) Math.pow(3, a - 1) * 4;
+        }
+        return (int) Math.pow(3, a) * 2;
+    }
+
+}
