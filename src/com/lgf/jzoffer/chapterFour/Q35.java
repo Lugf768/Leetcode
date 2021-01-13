71f16074dcb9582601c841ae49184fcbd3b6ad1c
LGF <lgf768561@163.com>
2021-01-13 11:11:42 +0800

    复杂链表的复制

---
+++ src/com/lgf/jzoffer/chapterFour/Q35.java
@@ -0,0 +1,45 @@
+package com.lgf.jzoffer.chapterFour;
+
+import java.util.HashMap;
+
+/**
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，
+ * 还有一个 random 指针指向链表中的任意节点或者 null。
+ */
+public class Q35 {
+
+
+    public Node copyRandomList(Node head) {
+        HashMap<Node,Node> map = new HashMap<>(); //创建HashMap集合
+        Node cur=head;
+        //复制结点值
+        while(cur!=null){
+            //存储put:<key,value1>
+            map.put(cur, new Node(cur.val)); //顺序遍历，存储老结点和新结点(先存储新创建的结点值)
+            cur=cur.next;
+        }
+        //复制结点指向
+        cur = head;
+        while(cur!=null){
+            //得到get:<key>.value2,3
+            map.get(cur).next = map.get(cur.next); //新结点next指向同旧结点的next指向
+            map.get(cur).random = map.get(cur.random); //新结点random指向同旧结点的random指向
+            cur = cur.next;
+        }
+
+        //返回复制的链表
+        return map.get(head);
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
