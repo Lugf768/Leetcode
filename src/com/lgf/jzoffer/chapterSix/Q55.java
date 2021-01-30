6c9e3493374e7e4ece0107e71efd34701e8b0c40
LGF <lgf768561@163.com>
2021-01-30 09:40:05 +0800

    二叉树的深度

---
+++ src/com/lgf/jzoffer/chapterSix/Q55.java
@@ -0,0 +1,54 @@
+package com.lgf.jzoffer.chapterSix;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ */
+public class Q55 {
+
+    //深度优先遍历
+    public int maxDepth1(TreeNode root) {
+
+        if(root == null){
+            return 0;
+        }
+        int left = maxDepth1(root.left);
+        int right = maxDepth1(root.right);
+        return Math.max(left+1, right+1);
+    }
+
+    //层序遍历（BFS）
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            res++;
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+        }
+        return res;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
