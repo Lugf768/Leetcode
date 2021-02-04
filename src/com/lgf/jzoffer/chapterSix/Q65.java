88ffdf02be408e26f8488783acda7b10b120fed8
LGF <lgf768561@163.com>
2021-02-04 09:25:06 +0800

    不用加减乘除做加法

---
+++ src/com/lgf/jzoffer/chapterSix/Q65.java
@@ -0,0 +1,17 @@
+package com.lgf.jzoffer.chapterSix;
+
+/**
+ * 写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+ */
+public class Q65 {
+
+    //https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/solution/mian-shi-ti-65-bu-yong-jia-jian-cheng-chu-zuo-ji-7/
+    public int add(int a, int b) {
+        while(b != 0) { // 当进位为 0 时跳出
+            int c = (a & b) << 1;  // c = 进位
+            a ^= b; // a = 非进位和
+            b = c; // b = 进位
+        }
+        return a;
+    }
+}
